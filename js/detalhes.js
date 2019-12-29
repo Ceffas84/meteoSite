e7defeb5dacfbd6b49431c9e85ab06bde4593ee3
Simão Pedro <simao.s.pedro@gmail.com>
2019-12-29 18:41:59 +0000

    Pequenas alteracoes.

--- js/detalhes.js
+++ js/detalhes.js
@@ -18,25 +18,30 @@ $(function () {
         method: 'GET',
         url: pedido
     }).done(function (msg) {
-        Object.keys(msg).forEach(function (key){
-            if(key === "name"){
-                $('#nome_cidade').text(msg[key]);
-            }
-        });
-        Object.keys(msg.main).forEach(function(key){
-            var item_param_clone = item_param.clone();
-            var item_valor_clone = item_valor.clone();
-            $('.param', item_param_clone).text(key);
-            if (key === "temp" || key === "feels_like" || key === "temp_min" || key === "temp_max"){
-                var celcius = parseInt(converter_Kelvin_to_Celcius(msg.main[key]));
-                $('.valor', item_valor_clone).text(celcius + " C.");
-            } else {
-                $('.valor', item_valor_clone).text(msg.main[key]);
-            }
-
-            $('.detalhes_tempo_param').append(item_param_clone);
-            $('.detalhes_tempo_valor').append(item_valor_clone);
-        });
+        if(typeof Storage !== "undefined"){
+            //codigo para localStorage
+            const msgString = JSON.stringify(msg);
+            Object.keys(msg).forEach(function (key){
+                if(key === "name"){
+                    $('#nome_cidade').text(msg[key]);
+                }
+            });
+            Object.keys(msg.main).forEach(function(key){
+                var item_param_clone = item_param.clone();
+                var item_valor_clone = item_valor.clone();
+                $('.param', item_param_clone).text(key);
+                if (key === "temp" || key === "feels_like" || key === "temp_min" || key === "temp_max"){
+                    var celcius = parseInt(converter_Kelvin_to_Celcius(msg.main[key]));
+                    $('.valor', item_valor_clone).text(celcius + " C.");
+                } else {
+                    $('.valor', item_valor_clone).text(msg.main[key]);
+                }
+                $('.detalhes_tempo_param').append(item_param_clone);
+                $('.detalhes_tempo_valor').append(item_valor_clone);
+            });
+        } else {
+            alert("O browser que esta a utilizar nao suporta Web Storage.");
+        }
     })
 });
 
@@ -49,3 +54,15 @@ function converter_Celcius_to_Kelvin(celcius_temp) {
     var kelvin = celcius_temp + diferenca_kelvin;
     return kelvin;
 }
+
+/*
+function converte_temperatura(arr) {
+
+}
+var arrTemp = $('.valor').toArray();
+arrTemp.forEach(function (temp){
+    console.log("Temperatura em C: " + temp);
+    arrTemp[temp] = converter_Celcius_to_Kelvin(temp);
+    console.log("Temperatura em K: " + arrTemp[temp]);
+});
+ */
