bc235dce695cf2e69097b00ca85823788a161b5d
2190787 <2190787@my.ipleiria.pt>
2019-12-30 21:00:16 +0000

    JSON Previsão significativa - Alteracao Bootstrap

--- js/significativa.js
+++ js/significativa.js
@@ -1,37 +1,35 @@
 const API_KEY = "APPID=5f641b8ef2e6971af3d88024c6489ebf";
 const API_URL = "http://api.openweathermap.org/data/2.5/forecast?q=London,us&";
-//const id_dtht = [4, 12, 20, 28, 36];
 
-var st_dia = null;
+var item_hora=null;
 
-$(function () {
-    st_dia = $('.dia').clone();
 
-    var search_url = API_URL + API_KEY;
-    $('.lista_dias').html('');
-
+$(function(){
+    item_hora=$(".hora").clone();
+    $(".linha_hora").html("");
 
     $.ajax({
-        method: "GET",
-        url: search_url
+        method: 'GET',
+        url: API_URL+API_KEY
+    }).done(function(msg){
 
-    }).done(function (msg) {
-        msg.list.forEach(function (element) {
-            var data = element.dt_txt;
-            var temp = element.main.temp;
-            var press = element.main.pressure;
-            var hum = element.main.humidity;
-
-            var div_dia = st_dia.clone;
-
-            $('.temperatura', div_dia).text(element.main.temp);
-
-            $('.lista_dias').append(div_dia);
-
-            console.log(element.main);
-            console.log(temp);
-            console.log(press);
-            console.log(hum);
+        count=0;
+        msg.list.forEach(function (elemento) {
+            if (count<=9) {
+                var item_hora_clone=item_hora.clone();
+                console.log(count);
+                var dataehora=elemento.dt_txt.split(" ");
+                var hora=dataehora[1];
+                console.log(hora);
+                count++;
+            }
+            $(".hora").text(hora);
+            $(".linha_hora").append(item_hora_clone);
         })
+
     })
+
 });
+
+
+
