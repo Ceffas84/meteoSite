b7c819dc25e7fac7636ac014b2786e3da8ae3a07
2190787 <2190787@my.ipleiria.pt>
2019-12-31 12:12:47 +0000

    JSON Previsão significativa - Alteracao tabela

--- js/significativa.js
+++ js/significativa.js
@@ -1,48 +1,50 @@
 const API_KEY = "APPID=5f641b8ef2e6971af3d88024c6489ebf";
 const API_URL = "http://api.openweathermap.org/data/2.5/forecast?q=London,us&";
 
-var item_hora=null;
-var linha_dia=null;
+var item_hora = null;
+var item_dia = null;
+var item_next_day = null;
 
-
-$(function(){
-    item_hora=$(".hora").clone();
-    $(".linha_hora").html("");
-    linha_dia=$(".coluna_dia").clone();
-    $(".linha_dia").html("");
+$(function () {
+    item_hora = $('.coluna_hora').clone();
+    item_dia = $('.coluna_dia').clone();
+    item_next_day = $('.linha_dia').clone();
 
     $.ajax({
         method: 'GET',
-        url: API_URL+API_KEY
-    }).done(function(msg){
-        var item_hora_clone=item_hora.clone();
-        $(".valor", item_hora_clone).text("#");
-        $(".linha_hora").append(item_hora_clone);
-        let i;
-        for (i=0; i<8; i++) {
-            var elemento=msg.list[i];
-            item_hora_clone=item_hora.clone();
-            console.log(item_hora_clone);
-            var dataehora=elemento.dt_txt.split(" ");
-            var hora=dataehora[1];
-            console.log(hora);
-            $(".valor", item_hora_clone).text((hora.split(":"))[0]);
-            $(".linha_hora").append(item_hora_clone);
+        url: API_URL + API_KEY
+    }).done(function (msg) {
+        var item_hora_clone = item_hora.clone();
+        $('.conteudo_hora').text("#");
+        //$('.linha_hora').append(item_hora_clone);
+        for (let i = 0; i < 8; i++) {
+            var elemento = msg.list[i];
+            item_hora_clone = item_hora.clone();
+            var dataehora = elemento.dt_txt.split(" ");
+            var hora = dataehora[1];
+            //console.log(hora);
+            $('.conteudo_hora', item_hora_clone).text((hora.split(":"))[0]);
+            $('.linha_hora').append(item_hora_clone);
         }
-        for (i=0;i<40;i+8){
-            var elemento=msg.list[i];
-            linha_dia_clone=linha_dia.clone();
-            console.log(linha_dia_clone);
-            var dataehora=elemento.dt_txt.split(" ");
-            var data=dataehora[0];
-            console.log(data);
-            $(".data", linha_dia_clone).text(data);
-            $(".linha_dia").append(linha_dia_clone);
+        for (let i = 0; i < 5; i++) {
+            $('.conteudo_dia_h').text("Proximas 24H");
+            for (let j = 0; j < 8; j++) {
+                    var elemento = msg.list[j];
+                    item_dia_clone = item_dia.clone();
+                    var dataehora = elemento.dt_txt.split(" ");
+                    var data = dataehora[0];
+                    console.log(data);
+                    $('.conteudo_dia').text(data);
+                    $('.linha_dia').append(item_dia_clone);
+            }
+            if (i < 4) {
+                var item_next_day_clone = item_next_day.clone();
+                $('.dados_dia').append(item_next_day_clone);
+            }
+        }
 
-        }
     });
 
 });
-
 
 
