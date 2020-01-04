a0642a7ea683189427952a987dce45a2af7cab50
Simão Pedro <simao.s.pedro@gmail.com>
2020-01-04 10:50:09 +0000

    Adicinada Google Places

--- js/pedido.js
+++ js/pedido.js
@@ -7,6 +7,17 @@ if(typeof Storage !== "undefined"){
     alert("Web Storage não suportado.");
 }
 
+var autocomplete;
+var place;
+
+$(function inicilizar_autocomplete() {
+    var input = document.getElementById('searchTextField');
+    autocomplete = new google.maps.places.Autocomplete(input);
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        pesquisar();
+    });
+});
+
 
 const API_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
 const API_FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?q=";
@@ -14,10 +25,18 @@ const API_KEY = "&APPID=5f641b8ef2e6971af3d88024c6489ebf";
 const ResponseOK = 200;
 
 function pesquisar() {
-    let city = $("#input_txt").val();
+    place = autocomplete.getPlace();
+    console.log(typeof place, place);
+    let city = place.name;
+    console.log(city);
+    let city_ID = place.getId();
+    console.log(city_ID);
+    let foto = place.photos;
+    console.log(foto);
     let unidade = "&units="+localStorage.getItem('unidade');
     let pedido_tempo_actual = API_WEATHER_URL+city+unidade+API_KEY;
 
+    /*
     $.ajax({
         method: 'GET',
         url: pedido_tempo_actual
@@ -33,4 +52,5 @@ function pesquisar() {
             }
         }
     })
+    */
 }
