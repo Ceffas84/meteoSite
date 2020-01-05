df8573fc78c078d3393b76e10e62f0645192f8a2
2190787 <2190787@my.ipleiria.pt>
2020-01-05 15:28:57 +0000

    JSON Previsão significativa - Alteracao tabela v8

--- js/main.js
+++ js/main.js
@@ -363,3 +363,26 @@ function descritivo_3h (div_dia){
         console.log("i -> ", i, " j-> ", j, "Data -> ", elemento.dt_txt, "Temp. -> ", temperatura, "Vel. Vento -> ", vento_vel, "Direcao Vento", vento_dir);
     }
 }
+
+let json_string = localStorage.getItem("tempo_atual");
+let json = JSON.parse(json_string);
+let item_media = null;
+
+let array_favoritos = [{}];
+
+
+
+function renderizar_pag_favoritos(){
+    item_media = $('.media').clone();
+    $('.media-list').html('');
+
+    console.log(json);
+    let item_media_clone = item_media.clone();
+    $('.cidade',item_media_clone).text(json.name);
+    $('.temp_max',item_media_clone).text(json.main.temp_max + '.º C');
+    $('.temp_min',item_media_clone).text(json.main.temp_min + '.º C');
+    $('.vento_vel',item_media_clone).text((json.wind.speed*3.6) + ' Kms/h');
+    $('.humidade',item_media_clone).text(json.main.humidity + '%');
+    $('.media-list').append(item_media_clone);
+
+}
